3c08ecb011ba4b7565d31c69dac4709bbf6e0d00
donbmt92 <71434503+donbmt92@users.noreply.github.com>
2024-03-23 02:03:37 +0700

    ServerSizeBar

--- src/components/navigation/navigation-action.tsx
+++ src/components/navigation/navigation-action.tsx
@@ -13,7 +13,7 @@ export const NavigationAction =  () => {
           onClick={() => onOpen("createServer")}
           className="group flex-items-center"
         >
-          <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-blue-500">
+          <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-[#d2d2d9] dark:bg-neutral-700 group-hover:bg-blue-500">
             <Plus
               className="group-hover:text-white transition text-blue-600 "
               size={25}
