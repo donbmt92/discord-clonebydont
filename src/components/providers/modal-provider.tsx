baa0b4a03b3c16b0b8b0f13b598927d2c3e11228
donbmt92 <71434503+donbmt92@users.noreply.github.com>
2024-03-25 17:31:41 +0700

    LeaveandDelete_Server

--- src/components/providers/modal-provider.tsx
+++ src/components/providers/modal-provider.tsx
@@ -6,6 +6,8 @@ import { InviteModal } from "@/components/modals/invite-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
+import { LeaveServerModal } from "@/components/modals/leave-server-modal";
+import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -21,10 +23,12 @@ export const ModalProvider = () => {
   return (
     <>
       <CreateServerModal />
-      <InviteModal/>
-      <EditServerModal/>
-      <MembersModal/>
-      <CreateChannelModal/>
+      <InviteModal />
+      <EditServerModal />
+      <MembersModal />
+      <CreateChannelModal />
+      <LeaveServerModal />
+      <DeleteServerModal />
     </>
   );
 };
--- src/hooks/use-modal-store.ts
+++ src/hooks/use-modal-store.ts
@@ -1,10 +1,17 @@
 import { Server } from "@prisma/client";
 import { create } from "zustand";
 
-export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel";
+export type ModalType =
+  | "createServer"
+  | "invite"
+  | "editServer"
+  | "members"
+  | "createChannel"
+  | "leaveServer"
+  | "deleteServer";
 
 interface ModalData {
-  server?: Server
+  server?: Server;
 }
 
 /* The `interface ModalStore` in the TypeScript code snippet defines the structure of the `ModalStore`
@@ -20,10 +27,10 @@ interface ModalStore {
 /* `useModal` is a custom hook created using Zustand library in TypeScript. It provides a
 state management solution for handling modals in an application. The `useModal` hook
 returns an object with properties `type`, `isOpen`, `onOpen`, and `onClose`. */
-export const  useModal = create<ModalStore>((set) => ({
+export const useModal = create<ModalStore>((set) => ({
   type: null,
   data: {},
   isOpen: false,
-  onOpen: (type, data = {} ) => set({ isOpen: true, type, data }),
+  onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
   onClose: () => set({ isOpen: false, type: null }),
 }));
