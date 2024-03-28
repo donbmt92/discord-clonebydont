3f9ff07ba98f9c1e2b3a0df7094e1b694d853288
donbmt92 <71434503+donbmt92@users.noreply.github.com>
2024-03-28 15:26:30 +0700

    Conversations to Memver Header

--- src/components/chat/chat-header.tsx
+++ src/components/chat/chat-header.tsx
@@ -19,6 +19,9 @@ export const ChatHeader = ({
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
       <MobileToggle serverId={serverId} />
+      {type === "conversation" && (
+        <UserAvatar src={imageUrl} className="w-8 h-8 md:w-10 mr-2" />
+      )}
       {type === "channel" && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
       )}
